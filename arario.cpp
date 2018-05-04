2d3ade0001e12cf9c9b1ee6b1b8772a131f7273e
Kclient <go4521304@gmail.com>
2018-05-04 17:56:44 +0900

    Initial commit

---
+++ arario.cpp
@@ -0,0 +1,170 @@
+#include <Windows.h>
+#include <tchar.h>
+
+#define nWitdth 800
+#define nHeight 800
+
+#define MinSize 10;	// 주인공원 / 바이러스 최소 사이즈
+#define FoodSize 5;	// 먹이 사이즈
+
+typedef struct PlayerCircle {
+
+	PlayerCircle* Parent;	// 부모노드
+
+	int size;	// 반지름
+
+	int x, y;	// 중심 좌표
+
+	PlayerCircle* Child1;
+	PlayerCircle* Child2;	// 자식노드들
+
+} Player;
+
+Player* First;
+
+HBRUSH	hBrush, oldBrush;
+HPEN hPen, oldPen;
+HDC memDC;
+HBITMAP hBitmap;
+
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = _T("Window Class Name");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+void Circle(Player*);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASSEX WndClass;
+	g_hInst = hInstance;
+
+	// 윈도우 클래스 구조체 값 설정
+	WndClass.cbSize = sizeof(WndClass);
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = (WNDPROC)WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.lpszMenuName = NULL;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	// 윈도우 클래스 등록
+	RegisterClassEx(&WndClass);
+
+	// 윈도우 생성
+	hWnd = CreateWindow
+	(lpszClass,
+		_T("Agar.io"),
+		WS_OVERLAPPED|WS_CAPTION|WS_SYSMENU,
+		10,
+		10,
+		nWitdth,
+		nHeight,
+		NULL,
+		(HMENU)NULL,
+		hInstance,
+		NULL);
+
+	// 윈도우 출력
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	// 이벤트 루프 처리
+	while (GetMessage(&Message, 0, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return Message.wParam;
+}
+
+void Circle(Player* p)
+{
+	if (p == NULL)
+		return;
+
+	Ellipse(memDC, p->x - p->size, p->y - p->size, p->x + p->size, p->y + p->size);
+
+	Circle(p->Child1);
+	Circle(p->Child2);
+}
+		
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hDC;
+
+	static int W, H;
+
+	int i;
+
+	switch (iMessage)
+	{
+	case WM_CREATE:
+		W = nWitdth - 15;
+		H = nHeight - 20;
+		First = (Player*)malloc(sizeof(Player));
+		First->Parent = NULL;
+		First->Child1 = NULL;
+		First->Child2 = NULL;
+
+		First->x = W / 2 + MinSize;
+		First->y = H / 2 + MinSize;
+		First->size = MinSize;
+		break;
+
+	case WM_PAINT:
+		hDC = BeginPaint(hWnd, &ps);
+		memDC = CreateCompatibleDC(hDC);
+		hBitmap = CreateCompatibleBitmap(hDC, W, H);
+
+		SelectObject(memDC, hBitmap);
+		
+		hPen = CreatePen(PS_SOLID, 2, RGB(50,205,50));
+		oldPen = (HPEN)SelectObject(memDC, hPen);
+		hBrush = CreateSolidBrush(RGB(144, 238, 144));
+		oldBrush = (HBRUSH)SelectObject(memDC, hBrush);
+
+		Rectangle(memDC, 0, 0, W, H);
+
+		for (i = 1; i < 10; ++i)
+		{
+			MoveToEx(memDC, 800 / 10 * i, 0, NULL);
+			LineTo(memDC, 800 / 10 * i, H);
+
+			MoveToEx(memDC,0, 800 / 10 * i, NULL);
+			LineTo(memDC, W, 800 / 10 * i);
+		}
+		SelectObject(memDC, oldPen);
+		SelectObject(memDC, oldBrush);
+		DeleteObject(oldPen);
+		DeleteObject(oldBrush);	// 판
+
+		hPen = CreatePen(PS_SOLID, 2, RGB(255, 102, 0));
+		oldPen = (HPEN)SelectObject(memDC, hPen);
+		hBrush = CreateSolidBrush(RGB(255, 153, 0));
+		oldBrush = (HBRUSH)SelectObject(memDC, hBrush);
+		Circle(First);
+		SelectObject(memDC, oldPen);
+		SelectObject(memDC, oldBrush);
+		DeleteObject(oldPen);
+		DeleteObject(oldBrush);	// 주인공 원 생성
+
+		BitBlt(hDC, 0, 0, W, H, memDC, 0, 0, SRCCOPY);
+		DeleteDC(memDC);
+		DeleteObject(hBitmap);
+		EndPaint(hWnd, &ps);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return (DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
